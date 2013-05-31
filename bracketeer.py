ae9417c122ea7cae1dcd5b64f97755c84fddfe3f
Seju <sbbidiwala@gmail.com>
2013-05-30 20:24:12 -0700

    Move Gambler model to root and add user_id field, add lookup code for users, split title into own container, and show User info on homepage.

--- bracketeer.py
+++ bracketeer.py
@@ -2,7 +2,10 @@ import os
 import urllib
 
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import ndb
+
+import gambler
 
 import jinja2
 import webapp2
@@ -15,8 +18,21 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
       user = users.get_current_user()
+      user_id = user.user_id()
 
-      template_values = {}
+      gambler_key = db.Key.from_path('Gambler', user_id)
+      gambler_obj = db.get(gambler_key)
+
+      if not gambler_obj:
+        gambler_obj = gambler.Gambler(
+            key_name=user_id, user_id=user_id, user_obj=user)
+        gambler_obj.put()
+
+      print gambler_obj
+      template_values = {
+          'first_name': gambler_obj.first_name,
+          'last_name': gambler_obj.last_name,
+          'nickname': user.nickname()}
 
       template = JINJA_ENVIRONMENT.get_template('templates/main.html')
       self.response.write(template.render(template_values))
--- gambler.py
+++ gambler.py
@@ -1,7 +1,9 @@
+from google.appengine.api import users
 from google.appengine.ext import db
 
 class Gambler(db.Model):
   first_name = db.StringProperty()
   last_name = db.StringProperty()
+  user_id = db.StringProperty()
   user_obj = users.User()
 
